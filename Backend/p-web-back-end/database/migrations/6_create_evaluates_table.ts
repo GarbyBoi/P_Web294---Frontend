8047fcab115aa4f2829bb512ac8898e79395116f
DevStackJunior <pv00wyv@eduvaud.ch>
2026-01-11 23:15:44 +0100

    Fix: Comments & Evaluations

--- Backend/p-web-back-end/database/migrations/6_create_evaluates_table.ts
+++ Backend/p-web-back-end/database/migrations/6_create_evaluates_table.ts
@@ -5,21 +5,12 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.specificType('note', 'tinyint')
+      table.float('note', 3, 1)
+      table.text('comment').nullable()
 
-      table
-      .integer('book_id')
-      .unsigned()
-      .references('id')
-      .inTable('books')
-      .onDelete('CASCADE') // Si on supprime 1 livre -> le/s évaluation/s est supprimé
+      table.integer('book_id').unsigned().references('id').inTable('books').onDelete('CASCADE') // Si on supprime 1 livre -> le/s évaluation/s est supprimé
 
-      table
-      .integer('user_id')
-      .unsigned()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE') // si on supprime le user on supprime ses évaluations
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE') // si on supprime le user on supprime ses évaluations
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
